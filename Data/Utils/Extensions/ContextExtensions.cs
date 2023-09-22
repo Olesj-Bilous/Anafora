9d47e86a6c237eca6a16a2d600dae801b1609480
Olesj-Bilous <olesjvbilous@gmail.com>
2023-09-22 17:11:29 +0200

    seed product model with meaningful names and values

--- Data/Utils/Extensions/ContextExtensions.cs
+++ Data/Utils/Extensions/ContextExtensions.cs
@@ -13,9 +13,7 @@ namespace AnaforaData.Utils.Extensions
         public static async Task SeedAsync(this DataContext context, UserManager<User> manager, string adminEmail, string adminPassword)
         {
             await context.SeedIdentityAsync(manager, adminEmail, adminPassword);
-            await context.SeedProductModelAsync<
-                string, ProductStringProperty, ProductStringValue, ProductModelStringValue, ProductType, ProductStringPropertyType
-            >(5, 3, () => Guid.NewGuid().ToString());
+            await context.SeedProductStringValuesAsync();
             await context.SaveChangesAsync().ConfigureAwait(false);
         }
 
@@ -34,64 +32,48 @@ namespace AnaforaData.Utils.Extensions
             }
         }
 
-        public static async Task SeedProductModelAsync<TValueType, TProperty, TValue, TProductValue, TType, TPropertyType>(
-            this DataContext context, int seedRange, int seedMultiplier, Func<TValueType> valGen
-        )
-            where TProperty : class, IDynamicProperty<Guid, TValueType>
-            where TValue : class, IDynamicValue<Guid, TValueType, TProperty>
-            where TProductValue: class, IDynamicModelValue<Guid, TProperty, TValueType, Product, TValue>
-            where TType: class, IDynamicType<Guid>
-            where TPropertyType : class, IDynamicPropertyType<Guid, TValueType, TProperty, TType>
+        public static async Task SeedProductStringValuesAsync(this DataContext context)
         {
-            var random = new Random();
-            
-            var types = new TType[seedRange];
-            for (var i = 0; i < seedRange; i++)
-            {
-                types[i] = (TType)Activator.CreateInstance(typeof(TType));
-                types[i].Name = Guid.NewGuid().ToString();
-            }
-
-            var propRange = seedRange * seedMultiplier;
-            var props = new TProperty[propRange];
-            var valRange = propRange * seedMultiplier;
-            var vals = new TValue[valRange];
-            for (var i = 0; i < propRange; i++)
-            {
-                props[i] = (TProperty)Activator.CreateInstance(typeof(TProperty));
-                props[i].Name = Guid.NewGuid().ToString();
-
-                var current = i * seedMultiplier;
-                for (var j = current; j < current + seedMultiplier; j++)
-                {
-                    vals[j] = (TValue)Activator.CreateInstance(typeof(TValue));
-                    vals[j].Property = props[i];
-                    vals[j].Value = valGen();
-                }
-            }
-
-            var modRange = propRange * seedMultiplier;
-            var models = new Product[modRange];
-            var modVals = new TProductValue[modRange * seedMultiplier];
-            for (var i = 0; i < modRange; i++)
-            {
-                models[i] = new Product();
-
-                var current = i * seedMultiplier;
-                for (var j = current; j < current + seedMultiplier; j++)
-                {
-                    modVals[j] = (TProductValue)Activator.CreateInstance(typeof(TProductValue));
-                    modVals[j].Model = models[i];
-                    modVals[j].Value = vals[random.Next(0, valRange)];
-                }
-            }
-
-            await context.Set<TType>().AddRangeAsync(types);
-            await context.Set<TProperty>().AddRangeAsync(props);
-            await context.Set<TValue>().AddRangeAsync(vals);
-            await context.Set<Product>().AddRangeAsync(models);
-            await context.Set<TProductValue>().AddRangeAsync(modVals);
-            await context.SaveChangesAsync();
+            ProductStringProperty color = new() { Name = "color" };
+            ProductStringProperty material = new() { Name = "material" };
+            ProductStringProperty size = new() { Name="size"};
+            ProductStringProperty scent = new() { Name = "scent" };
+            ProductStringValue[] colors = {
+                new() { Property = color, Value = "green" },
+                new() { Property = color, Value = "blue" },
+                new() { Property = color, Value = "red" }
+            };
+            ProductStringValue[] materials = {
+                new() { Property = material, Value = "leather" },
+                new() { Property = material, Value = "linnen" },
+                new() { Property = material, Value = "jeans" }
+            };
+            ProductStringValue[] sizes = {
+                new() { Property = size, Value = "small" },
+                new() { Property = size, Value = "medium" },
+                new() { Property = size, Value = "large" }
+            };
+            ProductStringValue[] scents = {
+                new() { Property = scent,Value="wood"},
+                new() { Property = scent,Value="vanille"},
+                new() { Property = scent,Value="summer"}
+            };
+            ProductType fashion = new() { Name = "fashion" };
+            ProductType perfumes = new() { Name = "perfumes" };
+            ProductStringPropertyType[] propertyTypes = {
+                new() { Property=scent, Type=perfumes},
+                new() { Property=size, Type=perfumes},
+                new() {Property=size,Type=fashion},
+                new() { Property=color,Type=fashion},
+                new() {Property=material,Type=fashion}
+            };
+            await context.AddRangeAsync(color, material, size, scent);
+            await context.AddRangeAsync(colors);
+            await context.AddRangeAsync(materials);
+            await context.AddRangeAsync(sizes);
+            await context.AddRangeAsync(scents);
+            await context.AddRangeAsync(fashion, perfumes);
+            await context.AddRangeAsync(propertyTypes);
         }
     }
 }
